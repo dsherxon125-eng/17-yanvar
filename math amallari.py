6f50e579925790681896b7ba95bf8d0200974ab8
Davronov Sherxon <dsherxon125@gmail.com>
2026-01-17 10:31:13 +0500

    Add files via upload

---
+++ math amallari.py
@@ -0,0 +1,15 @@
+import os
+os.system("cls")
+
+import math
+
+print(math.pow(5, 3))
+
+print(math.sqrt(121))
+
+print(math.log2(128))
+print(math.log(625, 5))
+
+print(math.floor(3.8))
+print(math.ceil(3.45))
+print(round(3.5))
---
+++ python edex hususiyatlari.py
@@ -0,0 +1,13 @@
+import os
+os.system("cls")
+
+text = "hello world"
+#print(text[3])
+#print(text[5:])
+#print(text[:5])
+#print(text[2:9])
+#print(text[2:10:2])
+#print(text[-1])
+#print(text[::-1])
+
+print(text*3)
